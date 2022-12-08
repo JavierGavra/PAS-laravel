7132a7f9d9ecfd68647e5450328b90e4ca1bcf9b
Biji_Tapir <bubblegaming431@gmail.com>
2022-12-08 07:33:54 +0700

    finishing

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0)
 });
 
 let menu = document.querySelector('#menu-icon');
